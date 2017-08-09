02d6baaf62932898115791947556d940abb6e1a0
veysiertekin <ertekin.veysi@gmail.com>
2017-08-09 19:51:59 +0300

    adds test cases for two and three

--- src/main/java/com/github/scturkey/RomanKataConverter.java
+++ src/main/java/com/github/scturkey/RomanKataConverter.java
@@ -2,8 +2,14 @@ package com.github.scturkey;
 
 public class RomanKataConverter {
 
-	public String convert(int decimal) {
-		return "I";
-	}
+    public String convert(int decimal) {
+        if (decimal == 1) {
+            return "I";
+        }
+        else if (decimal == 2) {
+            return "II";
+        }
+        return "III";
+    }
 
 }
--- src/test/java/com/github/scturkey/RomanKataConverterTest.java
+++ src/test/java/com/github/scturkey/RomanKataConverterTest.java
@@ -1,15 +1,26 @@
 package com.github.scturkey;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class RomanKataConverterTest {
-	
-	
+
+
     @Test
     public void one_shouldConvertTo_I() throws Exception {
-    	RomanKataConverter converter = new RomanKataConverter();
-    	assertEquals(converter.convert(1),"I");
+        RomanKataConverter converter = new RomanKataConverter();
+        assertEquals(converter.convert(1),"I");
     }
+    @Test
+    public void two_shouldConvertTo_II() throws Exception {
+        RomanKataConverter converter = new RomanKataConverter();
+        assertEquals(converter.convert(2),"II");
+    }
+    @Test
+    public void three_shouldConvertTo_III() throws Exception {
+        RomanKataConverter converter = new RomanKataConverter();
+        assertEquals(converter.convert(3),"III");
+    }
+
 }
