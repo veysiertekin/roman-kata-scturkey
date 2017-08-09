ebfec9f2b592f4adab1551a39063f5377c65f2bf
Erkan Erol <erkaner@netas.com.tr>
2017-08-09 20:20:33 +0300

    refactor repetative ones

--- src/main/java/com/github/scturkey/RomanKataConverter.java
+++ src/main/java/com/github/scturkey/RomanKataConverter.java
@@ -3,13 +3,13 @@ package com.github.scturkey;
 public class RomanKataConverter {
 
     public String convert(int decimal) {
-        if (decimal == 1) {
-            return "I";
-        }else if (decimal == 2) {
-            return "II";
-        }else if (decimal == 3){
-        	return "III";
-        }else if (decimal == 4){
+    	if (decimal<4){
+    		StringBuilder sb = new StringBuilder();
+    		for (int i=0;i<decimal;++i){
+    			sb.append("I");
+    		}
+    		return sb.toString();
+    	} else if (decimal == 4){
         	return "IV";
         }else if (decimal == 5){
         	return "V";
