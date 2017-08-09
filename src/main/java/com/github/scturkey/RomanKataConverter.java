dae75d3112ea3d7b11b35ccc189916cc5a73c189
veysiertekin <ertekin.veysi@gmail.com>
2017-08-09 20:27:29 +0300

    adds tests for ten's multiples

--- src/main/java/com/github/scturkey/RomanKataConverter.java
+++ src/main/java/com/github/scturkey/RomanKataConverter.java
@@ -3,22 +3,24 @@ package com.github.scturkey;
 public class RomanKataConverter {
 
     public String convert(int decimal) {
-    	if (decimal<4){
-    		StringBuilder sb = new StringBuilder();
-    		for (int i=0;i<decimal;++i){
-    			sb.append("I");
-    		}
-    		return sb.toString();
-    	} else if (decimal == 4){
-        	return "IV";
-        }else if (decimal == 5){
-        	return "V";
-        }else if (decimal == 6){
-        	return "VI";
-        }else {
-        	return "X";
+        if (decimal < 4) {
+            return getConcatRepetativeRomanNumber("I", decimal);
+        } else if (decimal == 4) {
+            return "IV";
+        } else if (decimal == 5) {
+            return "V";
+        } else if (decimal == 6) {
+            return "VI";
+        } else {
+            return getConcatRepetativeRomanNumber("X", decimal / 10);
         }
-        
     }
 
+    private String getConcatRepetativeRomanNumber(String romanNumber, int repeatCount) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < repeatCount; ++i) {
+            sb.append(romanNumber);
+        }
+        return sb.toString();
+    }
 }
