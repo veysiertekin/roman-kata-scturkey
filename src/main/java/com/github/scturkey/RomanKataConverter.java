54e7f12ee684378855418df221e338359aa95fef
veysiertekin <ertekin.veysi@gmail.com>
2017-08-09 21:05:31 +0300

    refactorings.

--- src/main/java/com/github/scturkey/RomanKataConverter.java
+++ src/main/java/com/github/scturkey/RomanKataConverter.java
@@ -3,7 +3,7 @@ package com.github.scturkey;
 public class RomanKataConverter {
 
     public String convert(int decimal) {
-    	if (decimal == 4) {
+        if (decimal == 4) {
             return "IV";
         } else if (decimal == 9) {
             return "IX";
@@ -24,12 +24,18 @@ public class RomanKataConverter {
         } else if (decimal < 4) {
             return getConcatRepetativeRomanNumber("I", decimal);
         } else if (decimal < 10) {
-            return "V"+convert(decimal-5);
-        } else if (decimal<40){
-            return getConcatRepetativeRomanNumber("X", decimal / 10)+convert(decimal%10);
-        } else if (decimal<100){
-            return "L"+convert(decimal-50);
-        } else if (decimal<1000){
+            return "V" + convert(decimal - 5);
+        } else if (decimal < 40) {
+            return getConcatRepetativeRomanNumber("X", decimal / 10) + convert(decimal % 10);
+        } else if (decimal < 50) {
+            return convert(40) + convert(decimal - 40);
+        } else if (decimal < 100) {
+            return convert(50) + convert(decimal - 50);
+        } else if (decimal < 400) {
+            return getConcatRepetativeRomanNumber("C", decimal / 100) + convert(decimal % 100);
+        } else if (decimal < 500) {
+            return convert(400) + convert(decimal - 400);
+        } else if (decimal < 1000) {
             return getConcatRepetativeRomanNumber("C", decimal / 100);
         } else {
             return getConcatRepetativeRomanNumber("M", decimal / 1000);
