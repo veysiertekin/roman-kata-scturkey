b4e68b11eefd8e926b2210977738efdce9d54b4a
veysiertekin <ertekin.veysi@gmail.com>
2017-08-09 20:44:17 +0300

    adds edge cases.

--- src/main/java/com/github/scturkey/RomanKataConverter.java
+++ src/main/java/com/github/scturkey/RomanKataConverter.java
@@ -7,12 +7,22 @@ public class RomanKataConverter {
             return getConcatRepetativeRomanNumber("I", decimal);
         } else if (decimal == 4) {
             return "IV";
+        } else if (decimal == 40) {
+            return "XL";
+        } else if (decimal == 90) {
+            return "XC";
+        } else if (decimal == 400) {
+            return "CD";
+        } else if (decimal == 900) {
+            return "CM";
         } else if (decimal == 5) {
             return "V";
         } else if (decimal == 50) {
             return "L";
         } else if (decimal == 500) {
             return "D";
+        } else if (decimal == 9) {
+            return "IX";
         } else if (decimal == 6) {
             return "VI";
         } else if (decimal<100){
